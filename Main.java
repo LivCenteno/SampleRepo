e2410e46bf13939dad730c5ae76dbb21cb0e670f
LivAndre <centeno2120959@mkt.ceu.edu.ph>
2024-12-13 13:15:30 +0800

    Sample Commits

---
+++ Main.java
@@ -0,0 +1,14 @@
+//Sample Repo
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Please Enter a Num: ");
+        int num = userInput.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.println(i);
+        }
+    }
+}
