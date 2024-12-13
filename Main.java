e43f0f6323dca2f9e2502aff8c140a2638a38346
LivAndre <centeno2120959@mkt.ceu.edu.ph>
2024-12-13 13:24:09 +0800

    Sample Commits3

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main{
     public static void main(String[] args) {
 
         Scanner userInput = new Scanner(System.in);
-        //ssamp
+        //ssampeqw
         System.out.println("Please Enter a Num: ");
         int num = userInput.nextInt();
         for (int i = 0; i <= num; i++) {
